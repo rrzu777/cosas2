5e38a86b72200dbf36ac2ea09310126d40378568
roberto_O <rrzu777@gmail.com>
2015-07-12 23:05:56 -0430

    otros
    otris

---
+++ otros/bootstrap3/inc/footer.php
@@ -0,0 +1,40 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-5">
+				<h3>PRÓXIMOS WORKSHOPS</h3>
+				<ul id="next-workshops">
+					<li><a href="detail.php">Título del primer workshop de pruebas</a></li>
+					<li><a href="detail.php">Título del segundo workshop de pruebas</a></li>
+				</ul>
+			</div>
+			<div class="col-xs-12 col-sm-7 col-md-6 col-md-offset-1">
+				<h3>NO TE PIERDAS NUESTRAS NOVEDADES</h3>
+				<form id="subscribe-form" action="">
+					<div class="row">
+						<div class="col-xs-7 col-sm-8">
+							<div class="form-group">
+								<label class="sr-only" for="email">tu correo electrónico</label>
+								<input type="text" id="email" name="email" class="form-control input-lg" placeholder="tu correo electrónico">
+							</div>
+						</div>
+						<div class="col-xs-5 col-sm-4">
+							<button class="btn btn-custom btn-lg">SUSCRIBIRME</button>
+						</div>
+					</div>
+				</form>
+			</div>
+			<div class="clearfix"></div>
+			<div class="col-xs-12 col-sm-5">
+				<h3>SOBRE LOS WORKSHOPS</h3>
+				<p>Estos workshps son el resultado de la suma de la ilusión y esfuerzos de <a href="#">Empresa</a>, <a href="#">Empresa 2</a> y <a href="#">Persona</a></p>
+				<p>Una apuesta diferente al panorama formativo nacional aunando workshops de primer nivel, impartidos por reconocidos profesionales con una experiencia de fin de semana, en un entorno diferente. </p>
+			</div>
+			<div class="col-xs-12 col-sm-7 col-md-6 col-md-offset-1">
+				<h3>TE GUSTA? CORRE LA VOZ</h3>
+				<p>Si te gusta esta filosofía de formación, o has acudido a uno de nuestros workshops y te hemos convencido, ayúdanos a promocionarlos por Internet para que otros puedan disfrutar de ellos:</p>
+				<!-- Insertar addthis -->
+			</div>
+		</div>
+	</div>
+</footer>
